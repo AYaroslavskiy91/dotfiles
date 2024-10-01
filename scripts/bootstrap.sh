6ff6084e601752c5d45b13f6f85918cc1c1cd355
Alex Yaroslavskiy <ayaroslavskiy@cadent.tv>
2024-10-01 12:38:48 -0400

    fixed bugs and tested

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -9,14 +9,22 @@ sourcepath() {
 
 safelink() {
     local SRC="$1"
-    local DEST="$home/`basename $SRC`"
+    local DEST="$HOME/`basename $SRC`"
+    if [ -e $DEST ]
+    then
+        echo "File $DEST exists, making backup"
+        cp "$DEST" "$DEST".backup
+    else
+        echo "No file found, making symlink"
+    fi
     ln -sf "$SRC" "$DEST"
 }
 
 SCRIPT_PATH=`sourcepath $0`
-DOTFILES=`dirname $SCRIPT_PATH`
+DOTFILES=`dirname $SCRIPT_PATH`/../homedotfiles
 
 for DOTFILE in `ls -A $DOTFILES`; do
     safelink "$DOTFILES/$DOTFILE"
+    echo $DOTFILE
 done
 
